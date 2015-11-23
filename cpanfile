3a05c73dda46d92c7d601bd6e99e0acc438d839f
Shoichi Kaji <skaji@cpan.org>
2015-11-24 01:20:38 +0900

    wip

--- cpanfile
+++ cpanfile
@@ -2,8 +2,12 @@ requires 'perl', '5.22.0';
 
 requires 'Amon2';
 requires 'DBD::SQLite';
+requires 'Daemon::Control';
+requires 'File::RotateLogs';
 requires 'Path::Tiny';
+requires 'Plack';
 requires 'Process::Status';
 requires 'Proclet';
+requires 'Starlet';
 requires 'Teng';
 requires 'YAML::Loader';
--- lib/Miniqlo/Runner.pm
+++ lib/Miniqlo/Runner.pm
@@ -3,25 +3,33 @@ use Miniqlo::Base;
 use Miniqlo::Web;
 use Miniqlo;
 
-use Proclet;
-use Plack::Loader;
-use JSON::PP ();
+use Daemon::Control;
 use Getopt::Long qw(:config no_auto_abbrev no_ignore_case bundling);
+use JSON::PP ();
+use Plack::Loader;
+use Proclet;
+use Try::Tiny;
+use Pod::Usage ();
 
 sub new ($class) { bless {}, $class }
 sub c ($self) { Miniqlo->context || Miniqlo->bootstrap }
+sub base_dir ($self) { $self->c->base_dir }
 sub host ($self) { $self->{host} }
 sub port ($self) { $self->{port} }
+sub max_workers ($self) { $self->{max_workers} }
 sub log_ttl ($self) { $self->{log_ttl} }
+sub daemonize ($self) { $self->{daemonize} }
 sub parse_options ($self, @argv) {
     local @ARGV = @argv;
     GetOptions
         "c|config=s"    => \my $config_file,
         "p|port=i"      => \my $port,
+        "max_workers=i" => \my $max_workers,
         "host=s"        => \my $host,
         "base_dir=s"    => \my $base_dir,
         "d|daemonize"   => \my $daemonize,
         "log_ttl_day=i" => \my $log_ttl_day,
+        "h|help"        => sub { Pod::Usage::pod2usage(1) },
     or exit 1;
     my $config = +{};
     if ($config_file) {
@@ -33,6 +41,7 @@ sub parse_options ($self, @argv) {
     $config->{web} ||= +{};
     $self->{port}        = $port        || $config->{web}{port}   || 5000;
     $self->{host}        = $host        || $config->{web}{host}   || '0.0.0.0';
+    $self->{max_workers} = $max_workers || $config->{web}{max_workers} || 10;
     $self->{log_ttl_day} = $log_ttl_day || $config->{log_ttl_day} || 14;
     $self->{daemonize}   = $daemonize   || $config->{daemonize}   || 0;
     $self->{log_ttl}     = $self->{log_ttl_day} * 24 * 60 * 60;
@@ -42,42 +51,77 @@ sub parse_options ($self, @argv) {
         $base_dir = Path::Tiny->new($config_file)->parent->child($base_dir);
     }
     $base_dir = Path::Tiny->new($base_dir)->absolute->stringify;
-    { no warnings; *Miniqlo::base_dir = sub { $base_dir } }
+    { no warnings qw(once redefine); *Miniqlo::base_dir = sub { $base_dir } }
     @ARGV;
 }
 sub run ($self, @argv) {
     $self = $self->new unless ref $self;
     @argv = $self->parse_options(@argv);
-    my $proclet = $self->_proclet;
+    my $subcmd = shift @argv or die "Need subcommand, try `$0 --help`\n";
+    my %valid = (start => 1, stop => 1, restart => 1, help => 1);
+    $valid{$subcmd} or die "Unknown subcommand '$subcmd'\n";
+    $subcmd eq "help" and Pod::Usage::pod2usage(1);
+
+    my $logger;
+    if ($self->daemonize) {
+        my $rotate = File::RotateLogs->new(
+            logfile  => $self->c->log_dir . "/_miniqlo/%Y%m%d.log",
+            linkname => $self->c->log_dir . "/_miniqlo/latest.log",
+            rotationtime => 24*60*60,
+            maxage => 0,
+            offset => Time::Piece->localtime->tzoffset,
+        );
+        $logger = sub { $logger->print(@_) };
+    }
+    my $proclet = $self->_proclet($logger ? (logger => $logger) : ());
+}
+
+sub daemon_control ($self, $program) {
+    Daemon::Control->new(
+        name => $0,
+        kill_timeout => 300,
+        stop_signals => ['TERM'],
+        program => $program,
+        pid_file => $self->base_dir . "/var/miniqlo.pid",
+        stdout_file => $self->base_dir . "/var/miniqlo.out",
+        stderr_file => $self->base_dir . "/var/miniqlo.out",
+        fork => 2,
+    );
 }
 
 sub _cleaner ($self) {
-    my $log_dir = $self->c->log_dir;
-    my $now = time;
-    my $deleted = 0;
+    my $log_dir = Path::Tiny->new($self->c->log_dir);
     sub {
-        Path::Tiny->new($log_dir)->visit(
+        my $now = time;
+        my $deleted = 0;
+        my @to_be_removed;
+        $log_dir->visit(
             sub {
                 my $path = shift;
-                return unless $path->is_file;
                 my $stat = $path->stat;
-                if ($now - $stat->mtime > $self->log_ttl) {
-                    eval { $path->remove };
-                    if ($@) {
-                        warn "Failed to unlink $path: $@\n";
-                    } else {
+                return unless $now - $stat->mtime > $self->log_ttl;
+                if ($path->is_dir) {
+                    push @to_be_removed, $path;
+                } else {
+                    try {
+                        $path->remove;
                         $deleted++;
-                    }
+                    } catch {
+                        warn "Failed to unlink $path: $@\n";
+                    };
                 }
             },
             { recursive => 1},
         );
+        for my $dir (@to_be_removed) {
+            rmdir $dir; # don't check
+        }
         warn "Deleted $deleted log files.\n";
     };
 }
 
-sub _proclet ($self) {
-    my $proclet = Proclet->new;
+sub _proclet ($self, $logger = undef) {
+    my $proclet = Proclet->new( $logger ? (logger => $logger) : () );
     for my $cron ($self->c->load_cron) {
         $proclet->service(
             tag => $cron->name,
@@ -92,6 +136,7 @@ sub _proclet ($self) {
             my $app = Miniqlo::Web->to_app;
             my $loader = Plack::Loader->load(
                 'Starlet', port => $self->port, host => $self->host,
+                max_workers => $self->max_workers,
             );
             $loader->run($app);
         },
